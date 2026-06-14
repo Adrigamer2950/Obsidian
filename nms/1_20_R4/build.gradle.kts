8f05f67ccd272ae7c1525db68e4b3b67dba54a1b
Adri <business@devadri.es>
2026-06-14 18:54:46 +0200

    Proper Java mismatch error fix

--- nms/1_20_R4/build.gradle.kts
+++ nms/1_20_R4/build.gradle.kts
@@ -11,4 +11,13 @@ kotlin {
     jvmToolchain(21)
 }
 
+configurations.named("reobf") {
+    attributes {
+        attribute(
+            TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE,
+            17
+        )
+    }
+}
+
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.REOBF_PRODUCTION
